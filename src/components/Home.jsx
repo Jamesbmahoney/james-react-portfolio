2906659dfe3323fc3fbb9390b06b888c0e426171
James Barry <jbm03003@gmail.com>
2022-06-01 14:26:15 -0400

    redid some coloring, added skills section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,11 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-gray-300">Hi, my name is</p>
+        <p className="text-[#800040]">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           James Mahoney
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#D10069]">
           I'm a Full Stack Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
@@ -20,7 +20,7 @@ const Home = () => {
           applications.
         </p>
         <div>
-          <button className="text-white  group border-2 px-6 py-3 my-2 flex items-center hover:bg-gray-400">
+          <button className="text-white  group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#D10069]">
             View Work
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />

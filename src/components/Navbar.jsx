32428443cf70a179a0eb4b4f8cb208db97622a65
James Barry <jbm03003@gmail.com>
2022-08-12 13:51:43 -0400

    added new resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -118,7 +118,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1AchRlpc1pRQfdAOG-ehbyR_7hT33TbCS/view?usp=sharing"
+              href="https://docs.google.com/document/d/1tneLQsd0RpM1Ww66yYRttdgpmIqwvnU-RhroS3OeMTY/edit?usp=sharing"
               target="_blank" rel="noreferrer"
             >
               Resume <BsFillPersonLinesFill size={30} />
